6723a54a0bde963117f845366efe4e853ea14ea1
Mateusz Kozak <koziu92@gmail.com>
2016-06-10 10:06:50 +0200

    Całość

--- PitagorasCalculate/Form1.Designer.cs
+++ PitagorasCalculate/Form1.Designer.cs
@@ -31,13 +31,22 @@
       this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
       this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
       this.panel1 = new System.Windows.Forms.Panel();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label1 = new System.Windows.Forms.Label();
       this.lA = new System.Windows.Forms.Label();
       this.lB = new System.Windows.Forms.Label();
-      this.label1 = new System.Windows.Forms.Label();
-      this.label2 = new System.Windows.Forms.Label();
-      this.label3 = new System.Windows.Forms.Label();
       this.lC = new System.Windows.Forms.Label();
       this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+      this.lArea = new System.Windows.Forms.Label();
+      this.lsAlfa = new System.Windows.Forms.Label();
+      this.lcAlfa = new System.Windows.Forms.Label();
+      this.lCircuit = new System.Windows.Forms.Label();
+      this.lcoAlfa = new System.Windows.Forms.Label();
+      this.ltAlfa = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label6 = new System.Windows.Forms.Label();
       ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
       ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
       this.panel1.SuspendLayout();
@@ -57,7 +66,7 @@
       this.numericUpDown1.Size = new System.Drawing.Size(63, 20);
       this.numericUpDown1.TabIndex = 0;
       this.numericUpDown1.Value = new decimal(new int[] {
-            1,
+            8,
             0,
             0,
             0});
@@ -76,7 +85,7 @@
       this.numericUpDown2.Size = new System.Drawing.Size(63, 20);
       this.numericUpDown2.TabIndex = 1;
       this.numericUpDown2.Value = new decimal(new int[] {
-            1,
+            6,
             0,
             0,
             0});
@@ -87,11 +96,38 @@
       this.panel1.Controls.Add(this.label3);
       this.panel1.Controls.Add(this.label2);
       this.panel1.Controls.Add(this.label1);
-      this.panel1.Location = new System.Drawing.Point(134, 29);
+      this.panel1.Location = new System.Drawing.Point(173, 29);
       this.panel1.Name = "panel1";
       this.panel1.Size = new System.Drawing.Size(363, 282);
       this.panel1.TabIndex = 2;
       this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(124, 217);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(25, 13);
+      this.label3.TabIndex = 2;
+      this.label3.Text = "b = ";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(165, 74);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(25, 13);
+      this.label2.TabIndex = 1;
+      this.label2.Text = "c = ";
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(36, 118);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(25, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "a = ";
       // 
       // lA
       // 
@@ -110,33 +146,6 @@
       this.lB.Size = new System.Drawing.Size(13, 13);
       this.lB.TabIndex = 4;
       this.lB.Text = "b";
-      // 
-      // label1
-      // 
-      this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(36, 118);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(25, 13);
-      this.label1.TabIndex = 0;
-      this.label1.Text = "a = ";
-      // 
-      // label2
-      // 
-      this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(165, 74);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(25, 13);
-      this.label2.TabIndex = 1;
-      this.label2.Text = "c = ";
-      // 
-      // label3
-      // 
-      this.label3.AutoSize = true;
-      this.label3.Location = new System.Drawing.Point(124, 217);
-      this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(25, 13);
-      this.label3.TabIndex = 2;
-      this.label3.Text = "b = ";
       // 
       // lC
       // 
@@ -165,19 +174,107 @@
       this.numericUpDown3.Size = new System.Drawing.Size(63, 20);
       this.numericUpDown3.TabIndex = 6;
       this.numericUpDown3.Value = new decimal(new int[] {
-            1,
+            10,
             0,
             0,
             0});
       this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
-      this.numericUpDown3.KeyUp += new System.Windows.Forms.KeyEventHandler(this.numericUpDown3_KeyUp);
-      this.numericUpDown3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.numericUpDown3_MouseDown);
+      // 
+      // lArea
+      // 
+      this.lArea.AutoSize = true;
+      this.lArea.Location = new System.Drawing.Point(29, 329);
+      this.lArea.Name = "lArea";
+      this.lArea.Size = new System.Drawing.Size(31, 13);
+      this.lArea.TabIndex = 8;
+      this.lArea.Text = "Pole:";
+      // 
+      // lsAlfa
+      // 
+      this.lsAlfa.AutoSize = true;
+      this.lsAlfa.Location = new System.Drawing.Point(170, 329);
+      this.lsAlfa.Name = "lsAlfa";
+      this.lsAlfa.Size = new System.Drawing.Size(59, 13);
+      this.lsAlfa.TabIndex = 9;
+      this.lsAlfa.Text = "Sinus alfa: ";
+      // 
+      // lcAlfa
+      // 
+      this.lcAlfa.AutoSize = true;
+      this.lcAlfa.Location = new System.Drawing.Point(159, 374);
+      this.lcAlfa.Name = "lcAlfa";
+      this.lcAlfa.Size = new System.Drawing.Size(70, 13);
+      this.lcAlfa.TabIndex = 10;
+      this.lcAlfa.Text = "Cosinus alfa: ";
+      // 
+      // lCircuit
+      // 
+      this.lCircuit.AutoSize = true;
+      this.lCircuit.Location = new System.Drawing.Point(13, 374);
+      this.lCircuit.Name = "lCircuit";
+      this.lCircuit.Size = new System.Drawing.Size(47, 13);
+      this.lCircuit.TabIndex = 11;
+      this.lCircuit.Text = "Obwód: ";
+      // 
+      // lcoAlfa
+      // 
+      this.lcoAlfa.AutoSize = true;
+      this.lcoAlfa.Location = new System.Drawing.Point(336, 374);
+      this.lcoAlfa.Name = "lcoAlfa";
+      this.lcoAlfa.Size = new System.Drawing.Size(75, 13);
+      this.lcoAlfa.TabIndex = 13;
+      this.lcoAlfa.Text = "Cotanges alfa:";
+      // 
+      // ltAlfa
+      // 
+      this.ltAlfa.AutoSize = true;
+      this.ltAlfa.Location = new System.Drawing.Point(342, 329);
+      this.ltAlfa.Name = "ltAlfa";
+      this.ltAlfa.Size = new System.Drawing.Size(69, 13);
+      this.ltAlfa.TabIndex = 12;
+      this.ltAlfa.Text = "Tanges alfa: ";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(109, 31);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(27, 13);
+      this.label4.TabIndex = 14;
+      this.label4.Text = "[cm]";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(109, 72);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(27, 13);
+      this.label5.TabIndex = 15;
+      this.label5.Text = "[cm]";
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(109, 115);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(27, 13);
+      this.label6.TabIndex = 16;
+      this.label6.Text = "[cm]";
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(507, 421);
+      this.ClientSize = new System.Drawing.Size(562, 421);
+      this.Controls.Add(this.label6);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.lcoAlfa);
+      this.Controls.Add(this.ltAlfa);
+      this.Controls.Add(this.lCircuit);
+      this.Controls.Add(this.lcAlfa);
+      this.Controls.Add(this.lsAlfa);
+      this.Controls.Add(this.lArea);
       this.Controls.Add(this.numericUpDown3);
       this.Controls.Add(this.lC);
       this.Controls.Add(this.lB);
@@ -186,7 +283,7 @@
       this.Controls.Add(this.numericUpDown2);
       this.Controls.Add(this.numericUpDown1);
       this.Name = "Form1";
-      this.Text = "Form1";
+      this.Text = "Pitagoras";
       ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
       this.panel1.ResumeLayout(false);
@@ -209,6 +306,15 @@
     private System.Windows.Forms.Label label1;
     private System.Windows.Forms.Label lC;
     private System.Windows.Forms.NumericUpDown numericUpDown3;
+    private System.Windows.Forms.Label lArea;
+    private System.Windows.Forms.Label lsAlfa;
+    private System.Windows.Forms.Label lcAlfa;
+    private System.Windows.Forms.Label lCircuit;
+    private System.Windows.Forms.Label lcoAlfa;
+    private System.Windows.Forms.Label ltAlfa;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Label label6;
   }
 }
 
--- PitagorasCalculate/Form1.cs
+++ PitagorasCalculate/Form1.cs
@@ -6,49 +6,63 @@ using System.Windows.Forms;
 namespace PitagorasCalculate
 {
   public partial class Form1 : Form
-  {
+  { 
     public Form1()
     {
       InitializeComponent();
+      Calculate((double)numericUpDown1.Value, (double)numericUpDown2.Value);
     }
 
-    private void CalculateCSide()
+    private void CalculateSide(double? _a = null, double? _b = null)
     {
-      var a = (double)numericUpDown1.Value;
-      var b = (double)numericUpDown2.Value;
-      var c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
-
-      numericUpDown3.Value = (decimal) c;
-
-      label1.Text = $"a = {a.ToString("####.##")}";
-      label3.Text = $"b = {b.ToString("####.##")}";
-      label2.Text = $"c = {numericUpDown3.Value.ToString("####.##")}";
-    }
-
-    private void CalculateBSide()
-    {
-      var a = (double)numericUpDown1.Value;
-      var c = (double)numericUpDown3.Value;
-      var b = Math.Sqrt(Math.Pow(c, 2) - Math.Pow(a, 2));
-
-      if (b < 1)
+      if (_a != null && _b != null)
       {
-        numericUpDown2.Value = (decimal) 2;
-        label3.Text = $"b = NIEPOPRAWNA WARTOŚĆ";
+        var a = (double) _a;
+        var b = (double) _b;
+        var c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+        numericUpDown3.Value = (decimal) c;
+
+        label1.Text = $"a = {a.ToString("####.##")}";
+        label3.Text = $"b = {b.ToString("####.##")}";
+        label2.Text = $"c = {numericUpDown3.Value.ToString("####.##")}";
       }
       else
       {
-        numericUpDown2.Value = (decimal)b;
-        label1.Text = $"a = {a.ToString("####.##")}";
-        label3.Text = $"b = {numericUpDown2.Value.ToString("####.##")}";
-        label2.Text = $"c = {c.ToString("####.##")}";
-      }
+        var a = (double)numericUpDown1.Value;
+        var c = (double)numericUpDown3.Value;
+        var b = Math.Sqrt(Math.Pow(c, 2) - Math.Pow(a, 2));
 
-     
+        if (b < 1 || double.IsNaN(b))
+        {
+          b =  (double)numericUpDown2.Value;
+          a = Math.Sqrt(Math.Pow(c, 2) - Math.Pow(b, 2));
+          if ((b < 1 || double.IsNaN(b)) || (a < 1 || double.IsNaN(a)))
+          {
+            label1.Text = @"a = NIEPOPRAWNA WARTOŚĆ";
+            label2.Text = @"c = NIEPOPRAWNA WARTOŚĆ";
+            label3.Text = @"b = NIEPOPRAWNA WARTOŚĆ";
+          }
+          else
+          {
+            numericUpDown2.Value = (decimal)b;
+            numericUpDown1.Value = (decimal)a;
+          }
+          
+        }
+        else
+        {
+          numericUpDown2.Value = (decimal)b;
+          label1.Text = $"a = {a.ToString("####.##")}";
+          label3.Text = $"b = {numericUpDown2.Value.ToString("####.##")}";
+          label2.Text = $"c = {c.ToString("####.##")}";
+        }
+      }
     }
+ 
     private void panel1_Paint(object sender, PaintEventArgs e)
     {
-      CalculateCSide();
+      CalculateSide();
 
       var l = e.Graphics;
       Pen pen = new Pen(Color.Black, 5);
@@ -59,28 +73,83 @@ namespace PitagorasCalculate
 
     private void numericUpDown1_ValueChanged(object sender, EventArgs e)
     {
-      CalculateCSide();
+      Calculate((double) numericUpDown1.Value, (double) numericUpDown2.Value);
     }
 
     private void numericUpDown2_ValueChanged(object sender, EventArgs e)
     {
-      CalculateCSide();
+      Calculate((double)numericUpDown1.Value, (double)numericUpDown2.Value);
     }
 
     private void numericUpDown3_ValueChanged(object sender, EventArgs e)
     {
-      
+      Calculate();
     }
 
-    private void numericUpDown3_MouseDown(object sender, MouseEventArgs e)
+    private void Calculate(double? _a = null, double? _b = null)
     {
-      CalculateBSide();
+      CalculateSide(_a, _b);
+      CalculateAre();
+      CalculateCircuite();
+      CalculateSinusAlfa();
+      CalculateCosinusAlfa();
+      CalculateTangesAlfa();
+      CalculateCotangesAlfa();
     }
 
-    private void numericUpDown3_KeyUp(object sender, KeyEventArgs e)
+    private void CalculateAre()
     {
-      CalculateBSide();
+      lArea.Text = $"Pole: {(numericUpDown1.Value*numericUpDown2.Value/2).ToString("####.##")} cm^2";
     }
-  
+
+    private void CalculateCircuite()
+    {
+      lCircuit.Text = @"Obwód: ";
+      lCircuit.Text += $"Obwód: {(numericUpDown1.Value + numericUpDown2.Value + numericUpDown3.Value).ToString("####.##")} cm";
+    }
+
+    private void CalculateSinusAlfa()
+    {
+      lsAlfa.Text = @"Sinus alfa: ";
+      lsAlfa.Text += $"0{(numericUpDown1.Value/numericUpDown3.Value).ToString("#.##")}";
+    }
+
+    private void CalculateCosinusAlfa()
+    {
+      lcAlfa.Text = @"Cosinus alfa: ";
+      lcAlfa.Text += $"0{(numericUpDown2.Value / numericUpDown3.Value).ToString("#.##")}";
+    }
+    private void CalculateTangesAlfa()
+    {
+      ltAlfa.Text = @"Tanges alfa: ";
+      var result = (numericUpDown1.Value / numericUpDown2.Value);
+      var resultText = result.ToString("####.##");
+      result = decimal.Parse(resultText);
+      if (result < 1 && result > -1)
+      {
+        ltAlfa.Text += $"0{(numericUpDown1.Value/numericUpDown2.Value).ToString("#.##")}";
+      }
+      else
+      {
+        ltAlfa.Text += $"{(numericUpDown1.Value / numericUpDown2.Value).ToString("#.##")}";
+
+      }
+    }
+    private void CalculateCotangesAlfa()
+    {
+      lcoAlfa.Text = @"Cotanges alfa: ";
+      var result = (numericUpDown2.Value/numericUpDown1.Value);
+      var resultText = result.ToString("####.##");
+      result = decimal.Parse(resultText);
+      if (result < 1 && result > -1)
+      {
+        lcoAlfa.Text += $"0{(result).ToString("#.##")}";
+      }
+      else
+      {
+        lcoAlfa.Text += $"{(result).ToString("#.##")}";
+      }
+    }
+
   }
 }
